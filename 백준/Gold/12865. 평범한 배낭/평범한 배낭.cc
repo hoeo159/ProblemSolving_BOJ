9fe7119ea9f7ea72ac6dc23070860df1e8d9491a
Minho_Baek_01 <58423056+hoeo159@users.noreply.github.com>
2026-04-09 17:20:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 76 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,37 +1,70 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-int dp[101][100001];
-vector<int> weight, cost;
-int N, M;
+int N, K;
 
-void solve(){
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= M; j++){
-            if(weight[i] <= j){
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]]+ cost[i]);
-            }
-            else{
-                dp[i][j] = dp[i-1][j];
-            }
-        }
+vector<pair<int, int>> item; // weight, value
+int dp[101][100001]; // i번째 인덱스에서 남은 무게가 j일 때
+
+int dfs(int index, int lastWeight)
+{
+    if(dp[index][lastWeight] != -1)
+    {
+        return dp[index][lastWeight];
     }
-    cout << dp[N][M] << "\n";
-    return;
+    
+    if(lastWeight == 0 || index == N) // 남은 무게가 0이면 못넣음
+    {
+        return 0;
+    }
+    else if(lastWeight < 0)
+    {
+        return -1;
+    }
+
+    int ret = 0;
+    int curWeight = item[index].first;
+    int curValue = item[index].second;
+    int local;
+    if(lastWeight - curWeight >= 0)
+    {
+        local = max(dfs(index + 1, lastWeight - curWeight) + curValue, dfs(index + 1, lastWeight));
+    }
+    else
+    {
+        local = dfs(index + 1, lastWeight);
+    }
+
+    if(ret < local)
+    {
+        ret = local;
+    }
+
+    dp[index][lastWeight] = ret;
+
+    return ret;
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0), cout.tie(0);
-    cin >> N >> M;
-    weight.resize(N + 1);
-    cost.resize(N + 1);
-    for(int i = 1; i <= N; i++){
-        cin >> weight[i] >> cost[i];
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> K;
+
+    for(int i = 0; i < N; i++)
+    {
+        int weight, value;
+        cin >> weight >> value;
+        item.push_back({weight, value});
+        memset(dp[i], -1, sizeof(dp[i]));
     }
-    solve();
+
+    int ret = dfs(0, K);
+    cout << ret << "\n";
     return 0;
 }
